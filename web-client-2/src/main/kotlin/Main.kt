d077cb3a8b0d0b1b07c0a94178f7ac12e7128c30
CesareIurlaro <cesareiurlaro@gmail.com>
2020-02-02 23:24:28 +0100

    added Spring project, WIP

--- web-client-2/src/main/kotlin/Main.kt
+++ web-client-2/src/main/kotlin/Main.kt
@@ -1,5 +1,4 @@
 import com.example.myapplication.modules.buildCommonKodein
-import com.example.myapplication.usecases.UseCase
 import com.github.lamba92.firebasemultiplatform.auth.FirebaseAuth
 import com.github.lamba92.firebasemultiplatform.core.FirebaseApp
 import com.github.lamba92.firebasemultiplatform.core.initializeApp
@@ -15,13 +14,13 @@ val container by Kodein.lazy {
     import(buildCommonKodein("", "", 8080))
     bind<FirebaseApp>() with singleton {
         FirebaseApp.initializeApp {
-            apiKey = "AIzaSyBxp-apzgy-EsK_wwrFh3rJPI32hjUWSA4",
-            authId = "unito-taas-project.firebaseapp.com",
-            databaseUrl = "https://unito-taas-project.firebaseio.com",
-            projectId = "unito-taas-project",
-            storageBucket = "unito-taas-project.appspot.com",
-            gcmSenderId = "848647809387",
-            applicationId = "1:848647809387:web:d35677d9266210b8d7c42e",
+            apiKey = "AIzaSyBxp-apzgy-EsK_wwrFh3rJPI32hjUWSA4"
+            authDomain = "unito-taas-project.firebaseapp.com"            // corretto?????
+            databaseUrl = "https://unito-taas-project.firebaseio.com"
+            projectId = "unito-taas-project"
+            storageBucket = "unito-taas-project.appspot.com"
+            gcmSenderId = "848647809387"
+            applicationId = "1:848647809387:web:d35677d9266210b8d7c42e"
             measurementId = "G-6HSSE0RCW1"
         }
     }
@@ -32,8 +31,43 @@ val container by Kodein.lazy {
 
 suspend fun main() {
 
-
-    UseCase
-    val container = my.
+    container.
 
 }
+
+
+//
+//fun main() {
+//
+//    println("Hello..")
+//
+//    doStuff()
+//
+//    println(".. World!")
+//}
+//
+//fun doStuff() = with(document) {
+//    getElementById("footer7-h")?.apply {
+//        id = "OVOLLO"
+//        innerHTML = create.h1 { +"LOOOOOOL" }.outerHTML
+//    }
+//}
+//
+//private fun veryFirstDoStuff() = with(document) {
+//
+//    val div = createElement("div").apply {
+//        appendChild(createElement("h1").apply { textContent = "Hello" })
+//        appendChild(createElement("button").apply {
+//            textContent = "Click me"
+//            addEventListener("click", {
+//                println("clicked")
+//            })
+//        })
+//    }
+//
+//    getElementById("root")?.apply {
+//        appendChild(div)
+//    }
+//}
+
+
